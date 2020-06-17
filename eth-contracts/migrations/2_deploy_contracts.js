bb6801595f7a5885f371d5ce03446b9076176aeb
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2020-06-17 16:16:42 +0200

    Changes in 2_deploy_contracts.js (still not working)

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -3,6 +3,8 @@ var verifier = artifacts.require("./verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(verifier);
-  deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier)
+    .then(() => {
+      return deployer.deploy(SolnSquareVerifier, verifier.address);
+    });
 };
