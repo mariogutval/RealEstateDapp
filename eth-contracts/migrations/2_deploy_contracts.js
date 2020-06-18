d5cf0d1d7bf9069092bad0fd297a5fe180cbcd86
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2020-06-18 02:49:08 +0200

    Added mint.js (Marioken has been deployed to rinkeby network)

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,9 +1,8 @@
 // migrating the appropriate contracts
-var verifier = artifacts.require("./Verifier.sol");
+var verifier = artifacts.require("./verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(verifier).then(() => {
-    deployer.deploy(SolnSquareVerifier, verifier.address)
-  });
-}
+module.exports = async function(deployer) {
+  await deployer.deploy(verifier);
+  await deployer.deploy(SolnSquareVerifier, verifier.address);
+};
